3ec7fe2ac1c50caf21196a9685f916f13c932566
Gino Galarza <slipkindo@gmail.com>
2023-08-24 00:39:58 -0500

    feat: added new relation with table pivot

--- app/Models/Artist.php
+++ app/Models/Artist.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Artist extends Model
 {
     use HasFactory;
+
+    public function musicalGenres(): BelongsToMany
+    {
+        return $this->belongsToMany(MusicalGenre::class);
+    }
 }
--- app/Models/MusicalGenre.php
+++ app/Models/MusicalGenre.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class MusicalGenre extends Model
 {
     use HasFactory;
+
+    public function artists(): BelongsToMany
+    {
+        return $this->belongsToMany(Artist::class);
+    }
 }
