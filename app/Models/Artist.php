2d1d0620ed157b055b142c5f826c8bad06ab1564
Gino Galarza <slipkindo@gmail.com>
2023-08-30 13:28:09 -0500

    feat: added relation members and artist in models

--- app/Models/Artist.php
+++ app/Models/Artist.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Artist extends Model
 {
@@ -17,11 +18,16 @@ class Artist extends Model
             ->withTimestamps();
     }
 
-    public function socialNetworks()
+    public function socialNetworks(): BelongsToMany
     {
         return $this->belongsToMany(SocialNetwork::class)
             ->using(ArtistSocialNetwork::class)
             ->withTimestamps()
             ->withPivot('url');
+    }
+
+    public function members(): HasMany
+    {
+        return $this->hasMany(Member::class);
     }
 }
