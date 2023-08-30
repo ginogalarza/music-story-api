fd9df6615dfd52e7adbeca12c1b99da1d9e1c70e
Gino Galarza <slipkindo@gmail.com>
2023-08-30 13:29:19 -0500

    perf: the return type of relationship artists

--- app/Models/SocialNetwork.php
+++ app/Models/SocialNetwork.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class SocialNetwork extends Model
 {
     use HasFactory;
 
-    public function artists()
+    public function artists(): BelongsToMany
     {
         return $this->belongsToMany(Artist::class)
             ->using(ArtistSocialNetwork::class)
