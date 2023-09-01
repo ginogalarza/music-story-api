d395677f09de1794295d0e067abc4db73b68185f
Gino Galarza <slipkindo@gmail.com>
2023-08-31 23:30:22 -0500

    feat: added new relationship with Song

--- app/Models/Album.php
+++ app/Models/Album.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Album extends Model
 {
@@ -13,5 +14,12 @@ class Album extends Model
     public function artist(): BelongsTo
     {
         return $this->belongsTo(Artist::class);
+    }
+
+    public function songs(): BelongsToMany
+    {
+        return $this->belongsToMany(Song::class)
+            ->using(AlbumSong::class)
+            ->withTimestamps();
     }
 }
