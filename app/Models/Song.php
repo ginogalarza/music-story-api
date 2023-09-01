faedee45443d912dd27ca215fb6ac1175cd01e7f
Gino Galarza <slipkindo@gmail.com>
2023-08-31 23:31:37 -0500

    feat: added relationship with concert, artist and album

--- app/Models/Song.php
+++ app/Models/Song.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Song extends Model
 {
     use HasFactory;
+
+    public function concerts(): BelongsToMany
+    {
+        return $this->belongsToMany(Concert::class)
+            ->withTimestamps()
+            ->using(ConcertSong::class);
+    }
+
+    public function artists(): BelongsToMany
+    {
+        return $this->belongsToMany(Artist::class)
+            ->withTimestamps()
+            ->using(ArtistSong::class);
+    }
+
+    public function albums(): BelongsToMany
+    {
+        return $this->belongsToMany(Album::class)
+            ->withTimestamps()
+            ->using(AlbumSong::class);
+    }
 }
